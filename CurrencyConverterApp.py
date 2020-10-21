be7efa86a109c38f0a391102de93439d95b546b5
sarwarkamran <38126219+sarwarkamran@users.noreply.github.com>
2020-10-21 18:09:12 +0500

    Add method for currency conversion calculations

--- CurrencyConverterApp.py
+++ CurrencyConverterApp.py
@@ -32,3 +32,30 @@ class Ui(QtWidgets.QDialog):
 
         self.to_currency_combo = self.findChild(QtWidgets.QComboBox, 'cb_to_currency')
         self.to_currency_combo.addItems(self.data['rates'].keys())
+        
+    def convertCurrency(self):
+        # This is executed when the button is pressed
+
+        self.currencyText = self.findChild(QtWidgets.QLineEdit, 'le_from_currency')
+        
+        from_amount = float(self.currencyText.text())
+        print(from_amount)
+        from_currency = str(self.from_currency_combo.currentText())
+        print(from_currency, self.data['rates'][from_currency])
+        to_currency = str(self.to_currency_combo.currentText())
+        print(to_currency, self.data['rates'][to_currency])
+
+        fraction = float(self.data['rates'][to_currency] / self.data['rates'][from_currency])
+
+        to_amount = fraction * from_amount
+
+        print(str(from_amount) + " " + from_currency + " are approximatly equals to " + str(to_amount) + " " + to_currency)
+        finalText = str(from_amount) + " " + from_currency + " = " + str(round(to_amount, 2)) + " " + to_currency
+        self.label = self.findChild(QtWidgets.QLabel, 'lbl_converted_currency')
+        self.label.setText(finalText)
+        self.label.adjustSize()
+        print('printButtonPressed')
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
